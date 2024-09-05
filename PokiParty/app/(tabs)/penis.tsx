795440fd20522d57bb36cdb72eadb0939b130112
JustNekoChris <103611606+JustNekoChris@users.noreply.github.com>
2024-09-04 23:53:02 -0700

    fixed subfolder issue

---
+++ PokiParty/app/(tabs)/penis.tsx
@@ -0,0 +1,10 @@
+
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+
+export default function Penis() {
+    return (
+        <ParallaxScrollView>
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        </ParallaxScrollView>
+    );
+};
