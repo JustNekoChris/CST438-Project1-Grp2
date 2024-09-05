625cfe4e1e970394392691e6ee16fd0aa7dc5421
Edward Leon <edleon761@gmail.com>
2024-09-04 21:57:35 -0700

    Checkpoint
    In the middle or integrating react w/ android

--- PokemonPartyPlanner/android/app/src/main/java/Word.java
+++ PokemonPartyPlanner/android/app/src/main/java/Word.java
@@ -1,9 +1,20 @@
 import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "word_table")
 public class Word {
 
-    private String word;
+    @PrimaryKey (autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
     private Word(@NonNull String word) {this.mWord = word;}
+
     private String getWord(){return this.mWord;}
-    
+
 }
---
+++ PokemonPartyPlanner/android/app/src/main/java/WordDao.java
@@ -0,0 +1,21 @@
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import java.util.ArrayList;
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deletaAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAlphabatizedWords();
+
+}
