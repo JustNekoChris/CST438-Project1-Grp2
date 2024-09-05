78a2685ff98c00093d06b2ce2609f4e4da46d32b
JudahSilva <jusilva@csumb.edu>
2024-09-05 01:31:50 -0700

    Add google oauth functionality

---
+++ PokiParty/app/index.tsx
@@ -0,0 +1,55 @@
+import { View } from "react-native";
+import { useEffect } from "react";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+
+export default function Index() {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        '157900039244-hju97cq2llua8ikt54rke1v81pldeg89.apps.googleusercontent.com',
+      offlineAccess: true,
+      forceCodeForRefreshToken: true,
+    });
+  });
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+
+      console.log(userInfo);
+    } catch (error: any) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        console.log('User cancelled the login flow');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        console.log('Signing in');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('Play services not available');
+      } else {
+        console.log('Some other error happened');
+        console.log(error.message);
+        console.log(error.code);
+      }
+    }
+  };
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <GoogleSigninButton
+        style={{width: 192, height: 48, marginTop: 30}}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={signIn}
+      />
+    </View>
+  );
+}
