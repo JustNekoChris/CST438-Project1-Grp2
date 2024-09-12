bb967cc243768883a8f514d67735c680342e1512
BetterCallSaulM <saulmachuca56@gmail.com>
2024-09-11 18:20:15 -0700

    You can now search for a pokemon and have it display some basic info

--- PokiParty/app/(tabs)/search.tsx
+++ PokiParty/app/(tabs)/search.tsx
@@ -7,18 +7,43 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function Search() {
+    const [searchBool, setSearchBool] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
+    const [pokemonData, setPokemonData] = useState(null);
+    
+    const toggleText = () => {
+        setSearchBool(prevBool => !prevBool);
+    };
+    
+    const onSearch = (input : string) => {
+        if (!searchBool) {
+            searchByName(input);
+        } else {
+            searchByType(input);
+        }
+    }
 
-    const search = async (pokemon : string) => {
+    const searchByName = async (pokemon : string) => {
         let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
         let response = await fetch(url);
         let data = await response.json();
         console.log(data);
+        console.log(data["sprites"]["front_default"])
+        setPokemonData(data);
+    }
+
+    const searchByType = async (type : string) => {
+        let url = `https://pokeapi.co/api/v2/type/${type}/`
+        let response = await fetch(url);
+        let data = await response.json();
+        console.log(data);
+        setPokemonData(data);
     }
 
     return (
         <ParallaxScrollView
-            headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }} >
+            headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }} 
+            headerImage={}>
             <ThemedView style={styles.titleContainer}>
                 <ThemedText type="title">Explore</ThemedText>
             </ThemedView>
@@ -27,12 +52,43 @@ export default function Search() {
             <View style={styles.searchContainer}>
                 <TextInput
                     style={styles.searchInput}
-                    placeholder="Search"
+                    placeholder={searchBool ? 'fairy' : 'squirtle'}
                     value={searchQuery}
                     onChangeText={setSearchQuery}
                 />
-                <Button title="Search" onPress={() => search(searchQuery)} />
+                <Button title="Search" onPress={() => onSearch(searchQuery)} />
             </View>
+            <View style={styles.buttonContainer}>
+                <Button 
+                    title={searchBool ? 'Search By Name' : 'Search By Type'} 
+                    onPress={toggleText}/>
+            </View>
+
+            {!pokemonData ? (
+                <ThemedText></ThemedText>
+            ) : (
+                <View>
+                    <View style={styles.buttonContainer}>
+                        <ThemedText>{pokemonData["name"]}</ThemedText>
+                        <ThemedText> Height: {pokemonData["height"]}</ThemedText>
+                        <Image 
+                        source={{ uri : pokemonData["sprites"]["front_default"]}}
+                        style={{width : 200, height : 200}}
+                        />
+                    </View>
+                    <ThemedText style={styles.center} type="title">Base Stats</ThemedText>
+                    <View style={styles.rows}>
+                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][0]["stat"]['name']} : {pokemonData["stats"][0]["base_stat"]} </ThemedText>
+                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][1]["stat"]['name']} : {pokemonData["stats"][1]["base_stat"]} </ThemedText>
+                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][2]["stat"]['name']} : {pokemonData["stats"][2]["base_stat"]} </ThemedText>
+                    </View>
+                    <View style={styles.rows}>
+                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][3]["stat"]['name']} : {pokemonData["stats"][3]["base_stat"]} </ThemedText>
+                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][4]["stat"]['name']} : {pokemonData["stats"][4]["base_stat"]} </ThemedText>
+                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][5]["stat"]['name']} : {pokemonData["stats"][5]["base_stat"]} </ThemedText>
+                    </View>
+                </View>
+            )}            
         </ParallaxScrollView>
     );
 };
@@ -47,6 +103,10 @@ const styles = StyleSheet.create({
     titleContainer: {
       flexDirection: 'row',
       gap: 8,
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        width: '100%'
     },
     searchContainer: {
         flexDirection: 'row',
@@ -69,9 +129,21 @@ const styles = StyleSheet.create({
         marginLeft: 8,
         fontSize: 16,
     },
-    buttonContainer: {
-        width: '40%', // Limit the width of the button to 40% of the screen width
-        alignSelf: 'center', // Center the button on the screen
-        marginTop: 10,
+    statColumns: {
+        flexDirection : 'column',
+        alignItems: 'center',
+        alignSelf: 'center',
+        margin: 15
     },
-  });
+    rows: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'center'
+    },
+    center: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        paddingTop: 50,
+        paddingBottom: 50
+    }
+});
