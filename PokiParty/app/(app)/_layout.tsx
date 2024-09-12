84e96879c07518fd9c83ccf1fe73cefa21bf2641
Edward Leon <edleon761@gmail.com>
2024-09-12 10:37:13 -0700

    Header is now hidden

--- PokiParty/app/(app)/_layout.tsx
+++ PokiParty/app/(app)/_layout.tsx
@@ -24,5 +24,10 @@ export default function AppLayout() {
   }
 
   // This layout can be deferred because it's not the root layout.
-  return <Stack />;
+
+  // Expo version of the Stack System
+  return <Stack 
+            screenOptions={{
+            headerShown: false
+            }}/>;
 }
