89252ee61f398e0ef81682a90f16a1df184ced61
BetterCallSaulM <saulmachuca56@gmail.com>
2024-09-20 21:04:27 -0700

    Modified search by type

--- PokiParty/app/(app)/search.tsx
+++ PokiParty/app/(app)/search.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { BasicBackButton } from '@/components/navigation/BackButton';
-import { StyleSheet, View, TextInput, Image, Modal, Button, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, TextInput, Image, Modal, Button, TouchableOpacity, ImageBackground } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -11,35 +11,42 @@ import { useSession } from '../../utils/DataContext';
 import AddOrRemoveButton from '@/components/AddOrRemoveButton';
 // Basic style sheet
 import { styles } from '@/assets/styles/searchPageStyleSheet';
+import { SearchByType } from '@/components/SearchByTypeView';
+import { DataTable } from 'react-native-paper';
 
 export default function Search() {
-    const [searchBool, setSearchBool] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
     const [modalData, setModalData] = useState(null);
     const [pokemonData, setPokemonData] = useState(null);
     const [pokemonTypeData, setPokemonTypeData] = useState(null);
     const [modalVisible, setModalVisible] = useState(false);
+    const [errorVisible, setErrorVisible] = useState(false);
+    const [typeSearchVisible, setTypeSearchVisible] = useState(false);
+    const [selectedType, setSelectedType] = useState('');
 
     const handlePress = async (pokemonName : string) => {
         await getModalData(pokemonName);
         setModalVisible(true);
     };
     
-    const toggleText = () => {
-        setSearchBool(prevBool => !prevBool);
+    const showModal = () => {
+        setTypeSearchVisible(prevBool => !prevBool);
     };
-    
-    const onSearch = (input : string) => {
-        if (!searchBool) {
-            searchByName(input);
-        } else {
-            searchByType(input);
-        }
-    }
+
+    const handleTypeChange = (value : string) => {
+        setSelectedType(value);
+    };
 
     const searchByName = async (pokemon : string) => {
         let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`
         let response = await fetch(url);
+        
+        if (response["status"] == 404) {
+            console.log("Not found");
+            setErrorVisible(true);
+            return;
+        }
+
         let data = await response.json();
         setPokemonData(data);
         setPokemonTypeData(null);
@@ -49,8 +56,16 @@ export default function Search() {
     const searchByType = async (type : string) => {
         let url = `https://pokeapi.co/api/v2/type/${type}/`
         let response = await fetch(url);
+
+        if (response["status"] == 404) {
+            console.log("Not found");
+            setErrorVisible(true);
+            return;
+        }
+
         let data = await response.json();
         setPokemonTypeData(data);
+        setTypeSearchVisible(false);
         setPokemonData(null);
         setModalData(null);
     }
@@ -58,8 +73,14 @@ export default function Search() {
     const getModalData = async (type : string) => {
         let url = `https://pokeapi.co/api/v2/pokemon/${type}/`
         let response = await fetch(url);
+
+        if (response["status"] == 404) {
+            console.log("Not found");
+            setErrorVisible(true);
+            return;
+        }
+
         let data = await response.json();
-        // console.log(data);
         setModalData(data);
         setPokemonData(null);
     }
@@ -75,9 +96,7 @@ export default function Search() {
     * 
     */
     const uriUrl = (s : string) =>{
-
         let portion = "";
-
         let start = 34;
         let count = start;
 
@@ -102,21 +121,64 @@ export default function Search() {
                 <View style={styles.statColumns}>
                     <TextInput
                         style={styles.searchInput}
-                        placeholder={searchBool ? 'fairy' : 'squirtle'}
+                        placeholder={'squirtle'}
                         value={searchQuery}
                         onChangeText={setSearchQuery}
                     />
                 </View>
                 <View style={styles.statColumns}>
-                    <Button title="Search" onPress={() => onSearch(searchQuery)} />
+                    <Button title="Search" onPress={() => searchByName(searchQuery)} />
                 </View>
             </View>
 
+            {/* Search By Type button */}
             <View style={styles.buttonContainer}>
                 <Button 
-                    title={searchBool ? 'Search By Name' : 'Search By Type'} 
-                    onPress={toggleText}/>
+                    title={'Search By Type'} 
+                    onPress={showModal}/>
             </View>
+
+            {/* Search by Type Modal */}
+            <Modal
+                animationType='slide'
+                transparent={true}
+                visible={typeSearchVisible}
+                onRequestClose={() => {
+                    setTypeSearchVisible(!typeSearchVisible)
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <SearchByType onValueChange={handleTypeChange}/>
+                            
+                        <View style={styles.rows}>
+                            <View style={styles.statColumns}>
+                                <Button title="Back" onPress={() => setTypeSearchVisible(false)} />
+                            </View>
+                            <View style={styles.statColumns}>
+                                <Button title="Search" onPress={() => searchByType(selectedType)} />
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
+
+            {/* Modal for when no results are found */}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={errorVisible}
+                onRequestClose={() => {
+                setModalVisible(!errorVisible);
+                }}
+            >
+                <View style={styles.modalContainer}>
+                    <View style={styles.modalContent}>
+                        <ThemedText style={styles.spacedDown}>Not Found</ThemedText>
+                        <Button title="Back" onPress={() => setErrorVisible(false)} />
+                    </View>
+                </View>
+            </Modal>
 
             {/* Search Result */}
             {!pokemonData ? (
@@ -130,19 +192,23 @@ export default function Search() {
                         style={{width : 200, height : 200}}
                         />
                         <ThemedText> Height: {pokemonData["height"]}</ThemedText>
+                        <ThemedText>Type(s):</ThemedText>
+                        {pokemonData.types?.map((type, index) => (
+                            <ThemedText>{type.type.name}</ThemedText>
+                        ))}
                     </View>
                     <ThemedText style={styles.center} type="title">Base Stats</ThemedText>
-                    <View style={styles.rows}>
-                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][0]["stat"]['name']} : {pokemonData["stats"][0]["base_stat"]} </ThemedText>
-                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][1]["stat"]['name']} : {pokemonData["stats"][1]["base_stat"]} </ThemedText>
-                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][2]["stat"]['name']} : {pokemonData["stats"][2]["base_stat"]} </ThemedText>
-                    </View>
-                    <View style={styles.rows}>
-                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][3]["stat"]['name']} : {pokemonData["stats"][3]["base_stat"]} </ThemedText>
-                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][4]["stat"]['name']} : {pokemonData["stats"][4]["base_stat"]} </ThemedText>
-                        <ThemedText style={styles.statColumns}> {pokemonData["stats"][5]["stat"]['name']} : {pokemonData["stats"][5]["base_stat"]} </ThemedText>
-                    </View>
-                    <View style={styles.center}>
+                    
+                    <DataTable>
+                        {pokemonData.stats?.map((stat, index) => (
+                        <DataTable.Row key={index} style={index % 2 === 0 ? styles.rowEven : styles.rowOdd}>
+                            <DataTable.Cell style={styles.statNameCell}>{stat.stat.name}</DataTable.Cell>
+                            <DataTable.Cell style={styles.statValueCell} numeric>{stat.base_stat}</DataTable.Cell>
+                        </DataTable.Row>
+                        ))}
+                    </DataTable>
+                    
+                    <View style={[styles.center, styles.spacedUp]}>
                         {/* new button that changes whther pokemon exists for user or not */}
                         <AddOrRemoveButton pokeID={pokemonData["id"]}/>
                         {/* <Button title='Add to Team' onPress={() => addPokemonToPC(pokemonData["id"], pokemonData["name"], pokemonData["sprites"]["front_default"])} /> */}
@@ -194,7 +260,7 @@ export default function Search() {
                         ) : (
                             <View style={styles.modalContainer}>
                                 <View style={styles.modalContent}>
-                                    <View>
+                                    <View style={styles.center}>
                                         <View style={styles.center}>
                                             <ThemedText>{modalData["name"]}</ThemedText>
                                             <Image 
@@ -202,20 +268,25 @@ export default function Search() {
                                             style={{width : 200, height : 200}}
                                             />
                                             <ThemedText> Height: {modalData["height"]}</ThemedText>
-                                        </View>
-                                        <ThemedText style={styles.center} type="title">Base Stats</ThemedText>
-                                        <View style={styles.rows}>
-                                            <ThemedText style={styles.statColumns}> {modalData["stats"][0]["stat"]['name']} : {modalData["stats"][0]["base_stat"]} </ThemedText>
-                                            <ThemedText style={styles.statColumns}> {modalData["stats"][1]["stat"]['name']} : {modalData["stats"][1]["base_stat"]} </ThemedText>
-                                            <ThemedText style={styles.statColumns}> {modalData["stats"][2]["stat"]['name']} : {modalData["stats"][2]["base_stat"]} </ThemedText>
-                                        </View>
-                                        <View style={styles.rows}>
-                                            <ThemedText style={styles.statColumns}> {modalData["stats"][3]["stat"]['name']} : {modalData["stats"][3]["base_stat"]} </ThemedText>
-                                            <ThemedText style={styles.statColumns}> {modalData["stats"][4]["stat"]['name']} : {modalData["stats"][4]["base_stat"]} </ThemedText>
-                                            <ThemedText style={styles.statColumns}> {modalData["stats"][5]["stat"]['name']} : {modalData["stats"][5]["base_stat"]} </ThemedText>
+                                            <ThemedText>Type(s):</ThemedText>
+                                            {modalData.types?.map((type, index) => (
+                                                <ThemedText>{type.type.name}</ThemedText>
+                                            ))}
                                         </View>
                                     </View>
-                                    <View style={styles.center, styles.rows}>
+                                    <View>
+                                        <ThemedText style={styles.center} type="title">Base Stats</ThemedText>
+                                
+                                        <DataTable>
+                                            {modalData.stats?.map((stat, index) => (
+                                            <DataTable.Row key={index} style={index % 2 === 0 ? styles.rowEven : styles.rowOdd}>
+                                                <DataTable.Cell style={styles.statNameCell}>{stat.stat.name}</DataTable.Cell>
+                                                <DataTable.Cell style={styles.statValueCell} numeric>{stat.base_stat}</DataTable.Cell>
+                                            </DataTable.Row>
+                                            ))}
+                                        </DataTable>
+                                    </View>
+                                    <View style={[styles.center, styles.rows]}>
                                         <Button title="Back" onPress={() => setModalVisible(false)} />
                                         <AddOrRemoveButton pokeID={modalData["id"]}/>
                                         {/* <Button title='Add to Team' onPress={() => addPokemonToPC(modalData["id"], modalData["name"], modalData["sprites"]["front_default"])} /> */}
